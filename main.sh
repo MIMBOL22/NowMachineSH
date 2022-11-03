bbb89bc25cddd92308d2beaec466a30705018e75
Александр <59563952+MIMBOL228@users.noreply.github.com>
2022-11-03 22:23:16 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,18 @@
-echo "Привет, устанавливаем веб сервера, просьба вводить пароли для root доступа"
-sudo add-apt-repository -y ppa:phpmyadmin/ppa
-sudo apt install -y tasksel nginx php7.4 php7.4-fpm php7.4-json php7.4-mysql php7.4-odbc phpmyadmin 
-sudo tasksel install lamp
-sudo apt remove -y apache2
-sudo apt remove
+
+if [ "$EUID" -ne 0 ] 
+    then sudo bash ./main.sh
+    exit
+    else echo "Привет, устанавливаем веб сервер"
+fi
+add-apt-repository -y ppa:phpmyadmin/ppa
+apt install -y mysql-server mysql-client nginx php7.4 php7.4-fpm php7.4-json php7.4-mysql php7.4-odbc phpmyadmin 
+printf "server {\n        listen 80 default_server;\n        root /var/www/html;\n        index index.php index.html;\n        location / {\n                try_files $uri $uri/ =404;\n        }\n        location ~ \.php$ {\n                include snippets/fastcgi-php.conf;\n                fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;\n        }\n}" >> /etc/nginx/sites-enabled/default
+systemctl restart nginx
+rm /var/www/html/index.html
+printf "<?php \necho 'Hello, World\!';" >> /var/www/html/index.php
+read -p "Имя пользователя для MySQL суперпользователя: " mysql_user
+read -s -p "Пароль для MySQL суперпользователя: " mysql_pass
+echo "\n"
+read -p "IP доступа для MySQL суперпользователя: [localhost]" mysql_host
+mysql -e "CREATE USER '${mysql_user}'@'${mysql_host:-localhost}' IDENTIFIED WITH mysql_native_password BY '${mysql_pass}';"
+echo "Готово ;)"
